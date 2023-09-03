14cdd4f33e018f593559e152c909f4dcf071422f
Petter Hoem Sletsjøe <petter.hoem.sletsjoe@semcon.com>
2023-09-03 23:56:15 +0200

    day2 1

--- src/main.rs
+++ src/main.rs
@@ -35,6 +35,96 @@ fn day1_run() {
     println!("Total calories: {}", total_calories);
 }
 
+#[derive(Debug)]
+enum RockPaperScissors {
+    Rock,
+    Paper,
+    Scissors,
+}
+
+impl RockPaperScissors {
+    fn beats(&self, other: &RockPaperScissors) -> bool {
+        match self {
+            RockPaperScissors::Rock => match other {
+                RockPaperScissors::Rock => false,
+                RockPaperScissors::Paper => false,
+                RockPaperScissors::Scissors => true,
+            },
+            RockPaperScissors::Paper => match other {
+                RockPaperScissors::Rock => true,
+                RockPaperScissors::Paper => false,
+                RockPaperScissors::Scissors => false,
+            },
+            RockPaperScissors::Scissors => match other {
+                RockPaperScissors::Rock => false,
+                RockPaperScissors::Paper => true,
+                RockPaperScissors::Scissors => false,
+            },
+        }
+    }
+    fn score(self, other: &RockPaperScissors) -> u32 {
+        if self.beats(other) {
+            self.self_score() + 6u32
+        } else if other.beats(&self) {
+            self.self_score()
+        } else {
+            self.self_score() + 3u32
+        }
+    }
+
+    fn self_score(self) -> u32 {
+        self.into()
+    }
+}
+
+impl Into<u32> for RockPaperScissors {
+    fn into(self) -> u32 {
+        match self {
+            RockPaperScissors::Rock => 1,
+            RockPaperScissors::Paper => 2,
+            RockPaperScissors::Scissors => 3,
+        }
+    }
+}
+
+impl Into<RockPaperScissors> for char {
+    fn into(self) -> RockPaperScissors {
+        match self {
+            'A' | 'X' => RockPaperScissors::Rock,
+            'B' | 'Y' => RockPaperScissors::Paper,
+            'C' | 'Z' => RockPaperScissors::Scissors,
+            _ => panic!("Invalid input"),
+        }
+    }
+}
+
+fn day2() {
+    println!("Hello, day 2");
+    let file = std::fs::read_to_string("input/2").unwrap();
+    let lines: Vec<&str> = file.split("\n").collect();
+
+    let mut total_score = 0;
+    let mut opponent: RockPaperScissors;
+    let mut me: RockPaperScissors;
+    for line in lines {
+        if line.len() == 0 {
+            continue;
+        }
+        //parse the line with two chars
+        let chars: Vec<char> = line.chars().collect();
+        println!("Chars: {:?}", chars);
+        opponent = chars[0].into();
+        me = chars[2].into();
+        println!("Opponent: {:?}, Me: {:?}", opponent, me);
+
+        let score = me.score(&opponent);
+        println!("Score: {}", score);
+        total_score += score;
+    }
+    println!("Total score: {}", total_score);
+}
+
 fn main() {
-    day1_run();
+    // day1_run();
+    day2();
 }
