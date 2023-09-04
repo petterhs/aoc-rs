44ab0f30975fdfbc1ac17536c5714eafd94b022e
Petter Hoem Sletsjøe <petter.hoem.sletsjoe@semcon.com>
2023-09-04 02:53:19 +0200

    day3 solved

--- src/day3.rs
+++ src/day3.rs
@@ -1,3 +1,5 @@
+use std::array;
+
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
 struct Priority(u32);
 
@@ -35,14 +37,41 @@ fn get_missplaced_item(s: &str) -> Option<char> {
     None
 }
 
+fn find_badge_item_priority(s1: &str, s2: &str, s3: &str) -> Option<u32> {
+    let mut array: [u8; 53] = [0; 53];
+
+    println!("{} {} {}", s1, s2, s3);
+    for c in s1.chars() {
+        let priority = Priority::new(c);
+        array[priority.0 as usize] = 1;
+    }
+
+    for c in s2.chars() {
+        let priority = Priority::new(c);
+        if array[priority.0 as usize] == 1 {
+            array[priority.0 as usize] = 2;
+        }
+    }
+
+    for c in s3.chars() {
+        let priority = Priority::new(c);
+        if array[priority.0 as usize] == 2 {
+            return Some(priority.0);
+        }
+    }
+
+    None
+}
+
 pub fn run() {
     println!("Day 3");
     let file = std::fs::read_to_string("input/3").unwrap();
     let lines: Vec<&str> = file.split("\n").collect();
 
     let mut sum: u32 = 0;
+    let mut badge_sum: u32 = 0;
 
-    for line in lines {
+    for line in &lines {
         println!("{}", line);
         if let Some(c) = get_missplaced_item(line) {
             sum += Priority::new(c).0;
@@ -50,6 +79,22 @@ pub fn run() {
         }
     }
     println!("Sum: {}", sum);
+
+    let mut count = 0;
+    //iterate 3 and 3 lines at a time
+    for i in (0..lines.len() - 1).step_by(3) {
+        let s1 = lines[i];
+        let s2 = lines[i + 1];
+        let s3 = lines[i + 2];
+        if let Some(priority) = find_badge_item_priority(s1, s2, s3) {
+            badge_sum += priority;
+            println!("{}: {}", i, priority);
+        }
+
+        count += 1;
+    }
+    println!("Badge sum: {}", badge_sum);
+    println!("Count: {}", count);
 }
 
 #[cfg(test)]
@@ -84,5 +129,20 @@ mod tests {
 
         s = "CrZsJsPPZsGzwwsLwLmpwMDw";
         assert_eq!(Some('s'), get_missplaced_item(s));
+    }
+
+    #[test]
+    fn test_group_badges() {
+        let s1 = "vJrwpWtwJgWrhcsFMMfFFhFp";
+        let s2 = "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL";
+        let s3 = "PmmdzqPrVvPwwTWBwg";
+
+        assert_eq!(Some(18), find_badge_item_priority(s1, s2, s3));
+
+        let s1 = "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn";
+        let s2 = "ttgJtRGJQctTZtZT";
+        let s3 = "CrZsJsPPZsGzwwsLwLmpwMDw";
+
+        assert_eq!(Some(52), find_badge_item_priority(s1, s2, s3));
     }
 }
