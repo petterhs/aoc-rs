e7fcebe70bc4734b82b3c371b69bd27d3b17f66e
Petter Hoem Sletsjøe <petter.hoem.sletsjoe@semcon.com>
2023-09-06 20:11:04 +0200

    solved day 8 part 2

--- src/day8.rs
+++ src/day8.rs
@@ -155,6 +155,83 @@ impl Trees {
         }
         println!("");
     }
+
+    fn iterate_2d_matrix(
+        &self,
+        start_row: usize,
+        start_col: usize,
+    ) -> (
+        impl Iterator<Item = &VisableTree>,
+        impl Iterator<Item = &VisableTree>,
+        impl Iterator<Item = &VisableTree>,
+        impl Iterator<Item = &VisableTree>,
+    ) {
+        let num_rows = self.0.len();
+        let num_cols = if num_rows > 0 { self.0[0].len() } else { 0 };
+
+        let up_iter = (0..start_row).rev().map(move |r| &self.0[r][start_col]);
+        let down_iter = (start_row + 1..num_rows).map(move |r| &self.0[r][start_col]);
+        let left_iter = (0..start_col).rev().map(move |c| &self.0[start_row][c]);
+        let right_iter = (start_col + 1..num_cols).map(move |c| &self.0[start_row][c]);
+
+        (up_iter, down_iter, left_iter, right_iter)
+    }
+
+    fn calculate_max_scenic_score(&mut self) -> i32 {
+        self.0
+            .iter()
+            .enumerate()
+            .map(|(row_index, row)| {
+                row.iter()
+                    .enumerate()
+                    .map(|(col_index, this_tree)| {
+                        let (up_iter, down_iter, left_iter, right_iter) =
+                            self.iterate_2d_matrix(row_index, col_index);
+
+                        let mut up_trees = 0;
+                        for tree in up_iter {
+                            up_trees += 1;
+                            if tree.height >= this_tree.height {
+                                break;
+                            }
+                        }
+
+                        let mut down_trees = 0;
+                        for tree in down_iter {
+                            down_trees += 1;
+                            if tree.height >= this_tree.height {
+                                break;
+                            }
+                        }
+
+                        let mut left_trees = 0;
+                        for tree in left_iter {
+                            left_trees += 1;
+                            if tree.height >= this_tree.height {
+                                break;
+                            }
+                        }
+
+                        let mut right_trees = 0;
+                        for tree in right_iter {
+                            right_trees += 1;
+                            if tree.height >= this_tree.height {
+                                break;
+                            }
+                        }
+
+                        let score = up_trees * down_trees * left_trees * right_trees;
+
+                        score
+                    })
+                    .max()
+                    .unwrap()
+                    .clone()
+            })
+            .max()
+            .unwrap()
+            .clone()
+    }
 }
 
 impl Display for Trees {
@@ -207,11 +284,20 @@ fn part1() {
     trees.visible_from_dir(&Direction::Right);
 
     println!("{}", trees);
-    println!("count: {}", count);
+    println!("Count: {}", count);
+}
+
+fn part2() {
+    let input = include_str!("../input/8");
+    let mut trees = parse_input(input);
+    let max = trees.calculate_max_scenic_score();
+
+    println!("Max scenic score: {}", max);
 }
 
 pub fn run() {
     part1();
+    part2();
 }
 
 #[cfg(test)]
@@ -265,5 +351,56 @@ mod tests {
         trees.visible_from_dir(&Direction::Right);
 
         assert!(count == 65);
+    }
+
+    #[test]
+    fn test_iterator() {
+        let input = include_str!("../input/test8");
+
+        let trees = parse_input(input);
+
+        let (up_iter, down_iter, left_iter, right_iter) = trees.iterate_2d_matrix(3, 3);
+
+        let up_iter = up_iter.collect::<Vec<&VisableTree>>();
+
+        assert!(up_iter.len() == 3);
+
+        let down_iter = down_iter.collect::<Vec<&VisableTree>>();
+
+        assert!(down_iter.len() == 1);
+
+        let left_iter = left_iter.collect::<Vec<&VisableTree>>();
+
+        assert!(left_iter.len() == 3);
+
+        let right_iter = right_iter.collect::<Vec<&VisableTree>>();
+
+        assert!(right_iter.len() == 1);
+
+        for tree in up_iter.iter() {
+            println!("up: {:?}", tree);
+        }
+
+        assert!(up_iter[0].height == 3);
+        assert!(up_iter[1].height == 1);
+        assert!(up_iter[2].height == 7);
+
+        assert!(down_iter[0].height == 9);
+
+        assert!(left_iter[0].height == 5);
+        assert!(left_iter[1].height == 3);
+        assert!(left_iter[2].height == 3);
+
+        assert!(right_iter[0].height == 9);
+    }
+
+    #[test]
+    fn test_scenic_score() {
+        let input = include_str!("../input/test8");
+
+        let mut trees = parse_input(input);
+        let max = trees.calculate_max_scenic_score();
+
+        assert_eq!(max, 8);
     }
 }
